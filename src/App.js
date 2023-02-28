dab9446bd90bd4f11d0b038e5eac15472f21a0a7
Your NameNikhilsingRajput <nikhilsingrajput2016@gmail.com>
2023-02-28 16:52:18 +0530

    signin page added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Sign_in from './component/signin';
 
 function App() {
   return (
     <div className="App">
-      
+      <Sign_in />
     </div>
   );
 }
---
+++ src/component/signin.js
@@ -0,0 +1,29 @@
+import "./signin.css";
+function Sign_in() {
+
+    return <div className="frontpage">
+        <div className="left-side">
+            <div className="welcome-text">Welcome Page</div>
+            <div className="below-text">One line text Will be here</div>
+            <div className="text-small">Sign in to continue access pages</div>
+            <div className="small-text">Don’t Have An Account?</div>
+            <button>Register</button>
+        </div>
+        <div className="right-side">
+            <div className="Inner-box">
+                <div className="Sign-in-heading">Sign In</div>
+                <div className="Below-heading">Sign in to continue access pages</div>
+                <div className="inp">
+                    <input placeholder="Email" type="email" />
+                </div>
+                <div className="inp">
+                    <input className="hov" placeholder="Password" type="password" />
+                </div>
+                <button className="signin-button">Sign In</button>
+            </div>
+        </div>
+    </div>
+
+}
+
+export default Sign_in
